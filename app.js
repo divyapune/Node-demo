3169ca858e9f80b808839d73f1c5a6482699c8be
divya <divyapune8410@gmail.com>
2026-03-24 15:09:16 +0530

    app.js updated

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("This is a devops-project page! for CI/CD Pipeline");
+  res.send(
+    "This is a simple Node.js application running in a Docker container.",
+  );
 });
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
